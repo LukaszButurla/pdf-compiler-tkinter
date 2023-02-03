e9c292aaf0ecfcaa7bd1a6b9d396c087b56a7ad3
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-03 13:03:16 +0100

    Create edit button and change window size

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class MainWindow:
         app = customtkinter.CTk()
         customtkinter.set_appearance_mode("dark")
         customtkinter.set_default_color_theme("blue")
-        app.geometry("400x400")
+        app.geometry("600x400")
 
         self.labelListOfFiles = customtkinter.CTkLabel(app, text="Lista plików:", justify="left")
         self.labelListOfFiles.place(relx=0.02, rely=0.15)
@@ -23,8 +23,11 @@ class MainWindow:
         buttonAdd = customtkinter.CTkButton(master=app, text="Dodaj", command=self.compiler.open_select_file_window)
         buttonAdd.place(relx=0.02, rely=0.05)
 
+        buttonEdit = customtkinter.CTkButton(master=app, text="Edytuj")
+        buttonEdit.place(relx=0.35, rely=0.05)
+
         buttonCompile = customtkinter.CTkButton(master=app, text="Kompiluj", command=self.compiler.mergePdf)
-        buttonCompile.place(relx=0.5, rely=0.05)
+        buttonCompile.place(relx=0.67, rely=0.05)
         app.mainloop()
 
 
