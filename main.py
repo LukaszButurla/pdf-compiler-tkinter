0d4063668bea73e163a6c89b0a293e4fe4586cdd
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-03 10:40:40 +0100

    Create main window

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+import customtkinter
+
+app = customtkinter.CTk()
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+app.geometry("400x400")
+
+
+
+app.mainloop()
