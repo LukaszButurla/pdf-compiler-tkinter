d5a2023ea59aa7342dc97026fab2bb18e9c09d9f
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-03 11:48:08 +0100

    Create function to change label's text

--- logic/compiler.py
+++ logic/compiler.py
@@ -1,12 +1,24 @@
 from tkinter import filedialog as fd
 
 class Compiler:
-    def __init__(self):
+    def __init__(self, labelFileList):
         self.allFiles = []
+        self.labelFileList = labelFileList
+        print(self.labelFileList)
 
     def open_select_file_window(self):
         selectedFiles = fd.askopenfilenames()
         for file in selectedFiles:
             self.allFiles.append(file)
+        self.set_label_name()
         print(self.allFiles)
         print(selectedFiles)
+
+    def set_label_name(self):
+        txt = "Lista plików:\n"
+        for file in self.allFiles:
+            txt += "{}\n".format(file)
+
+        self.labelFileList.configure(text=txt)
+
+    
