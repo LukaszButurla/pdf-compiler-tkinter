36670b58220a0011b85f72fc3efc05365b1f4ce6
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-03 10:48:26 +0100

    Add button and label with list of selected files

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 app.geometry("400x400")
 
-
+buttonAdd = customtkinter.CTkButton(master=app, text="Dodaj")
+buttonAdd.place(relx=0.5, rely=0.5)
+labelListOfFiles = customtkinter.CTkLabel(master=app, text="Lista plików:")
+labelListOfFiles.place(relx=0.5, rely=0.3)
 
 app.mainloop()
