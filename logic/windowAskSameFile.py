b52fd0755cfafd0fd648e827d258fd970dd35a7d
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-06 08:12:23 +0100

    Refactor select files to add function

--- logic/windowAskSameFile.py
+++ logic/windowAskSameFile.py
@@ -6,30 +6,39 @@ class WindowAskSameFile:
 
     def __init__(self, compiler):
         self.compiler = compiler
+        self.files = []
 
     def open_window(self, app, file):
+        self.files = file
+        self.selected = []
         self.windowAsk = Toplevel(app, background="#242424")
         self.windowAsk.geometry("500x250")
-        self.add_widgets_to_window(file)
+        self.add_widgets_to_window()
         
 
-    def add_widgets_to_window(self, file):
+    def add_widgets_to_window(self):
         labelInfo = customtkinter.CTkLabel(master=self.windowAsk, justify="center", text="Plik o podanej nazwie jest juz dodany. Czy chcesz dodać ponownie?")
         labelInfo.place(relx = 0.01, rely=0.3)
 
-        buttonAgree = customtkinter.CTkButton(master=self.windowAsk, text="Tak",command=partial(self.close_window, True, file))
+        buttonAgree = customtkinter.CTkButton(master=self.windowAsk, text="Tak",command=partial(self.select_if_add, True))
         buttonAgree.place(relx=0.1, rely=0.7)
 
-        buttonDecline = customtkinter.CTkButton(master=self.windowAsk, text="Nie", command=partial(self.close_window, False, file))
+        buttonDecline = customtkinter.CTkButton(master=self.windowAsk, text="Nie", command=partial(self.select_if_add, False))
         buttonDecline.place(relx=0.55, rely=0.7)
 
-    def close_window(self, state, file):
-        print(self.windowAsk)
+    def select_if_add(self, state):
         if state == False:
-            self.windowAsk.destroy()
-            
+            self.files.remove(self.files[0])
         
         elif state == True:
-            self.windowAsk.destroy()
-            self.compiler.add_file_from_window(file)
+            self.selected.append(self.files[0])
+            self.files.remove(self.files[0])
+
+        if len(self.files) == 0:
+            self.close_window(self.selected)
+
+
+    def close_window(self, file):
+        self.windowAsk.destroy()
+        self.compiler.add_file_from_window(file)
             
