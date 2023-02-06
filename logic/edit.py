d0195f858544b9731e7f9ec4b73b9924eec69940
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-06 09:10:48 +0100

    Add buttons and label

--- logic/edit.py
+++ logic/edit.py
@@ -8,7 +8,7 @@
 #     def open_edit_window(self):
 #         self.editWindow = Toplevel(self.window, background="#242424")
 #         self.editWindow.geometry("600x400")
-
+import customtkinter
 from tkinter import Frame
 
 class Edit:
@@ -20,4 +20,14 @@ class Edit:
         self.editWindow = Frame(self.window, background="#242124")
         # self.editWindow.geometry("600x400")
         self.editWindow.pack(fill="both", side="bottom", expand=True)
-        print("pack")
+        self.add_widgets()
+
+    def add_widgets(self):
+        buttonCancel = customtkinter.CTkButton(self.editWindow, text="Anuluj")
+        buttonCancel.place(relx=0.6, rely=0.85)
+
+        buttonAgree = customtkinter.CTkButton(self.editWindow, text="Potwierdź")
+        buttonAgree.place(relx=0.2, rely=0.85)
+
+        infoLabel = customtkinter.CTkLabel(self.editWindow, text="Wybierz pliki do usunięcia z listy")
+        infoLabel.place(relx=0.04, rely=0.03)
