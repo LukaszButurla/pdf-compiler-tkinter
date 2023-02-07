965e9021a83ca31b0d2a72f039022c78bce7ada3
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-07 10:23:03 +0100

    Add widgets to info page

--- Ui/info.py
+++ Ui/info.py
@@ -1,4 +1,6 @@
 from tkinter import Frame
+import customtkinter
+from tkinter import LEFT
 
 class InfoPage:
 
@@ -8,3 +10,14 @@ class InfoPage:
     def open_page(self):
         self.page = Frame(self.window, background="#242124")
         self.page.pack(fill="both", expand=True)
+        self.create_widgets()
+
+    def create_widgets(self):
+
+        labelCompany = customtkinter.CTkLabel(self.page,
+                                              text="Informacje firmy\nKancelaria Doradcy Podatkowego Super Nowa\nPiotr, Monika Maciejewski\nStrona internetowa: https://www.doradcy.net.pl\nE-mail: maciejewski@doradcy.net.pl\nNr tel. (32) 476 36 13",
+                                             justify = LEFT, font=("Arial", 20))
+        labelCompany.place(relx = 0.05, rely=0.05)
+
+        btnExit = customtkinter.CTkButton(self.page, text="Wyjdź")
+        btnExit.place(relx=0.4, rely=0.85)
