ff83f702cede16220fb9720c2bdef7d66f4d1fb6
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-07 11:13:40 +0100

    Style info page

--- Ui/info.py
+++ Ui/info.py
@@ -14,10 +14,19 @@ class InfoPage:
 
     def create_widgets(self):
 
-        labelCompany = customtkinter.CTkLabel(self.page,
-                                              text="Informacje firmy\nKancelaria Doradcy Podatkowego Super Nowa\nPiotr, Monika Maciejewski\nStrona internetowa: https://www.doradcy.net.pl\nE-mail: maciejewski@doradcy.net.pl\nNr tel. (32) 476 36 13",
-                                             justify = LEFT, font=("Arial", 18))
-        labelCompany.place(relx = 0.05, rely=0.05)
+        boxColor = "#242424"
+
+        boxFrame = Frame(self.page, background=boxColor)
+        boxFrame.place(relx = 0.06, rely=0.08, width=660, height=325)
+
+        labelCompanyName = customtkinter.CTkLabel(boxFrame,
+                                              text="Doradcy Podatkowi Piotr Maciejewski,\n Monika Maciejewska Spółka z ograniczoną\nodpowiedzialnością",
+                                              font=("Arial", 18))
+        labelCompanyName.place(relx = 0, rely = 0.2, width = 660, height=100)
+
+        labelCompanyContact = customtkinter.CTkLabel(boxFrame, text="Strona internetowa: www.doradcy.net.pl\nE-mail: maciejewski@doradcy.net.pl\nNr tel. (32) 476 36 13",
+                                                     font=("Arial", 18))
+        labelCompanyContact.place(relx = 0, rely = 0.5, width = 660, height=100)
 
         btnExit = customtkinter.CTkButton(self.page, text="Wyjdź", command=self.close_page)
         btnExit.place(relx=0.4, rely=0.85)
