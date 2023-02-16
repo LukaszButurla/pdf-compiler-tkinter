1809ef5f263e4b0184fcbb44395f13dd8d8be027
BIURO21\praktykant <lukasz.buturla.25@gmail.com>
2023-02-16 11:14:07 +0100

    Create frame with pages

--- logic/splitOptions.py
+++ logic/splitOptions.py
@@ -26,10 +26,37 @@ class SplitOptions:
         self.amountOfFilesSlider = customtkinter.CTkSlider(self.settingsFrame, command=self.update_slider, width=250, from_=1, to=amountOfPages, number_of_steps=amountOfPages - 1)
         self.amountOfFilesSlider.place(relx = 0.025, rely = 0.2)
 
-        self.amountOfFilesLabel = customtkinter.CTkLabel(self.settingsFrame, text = self.amountOfFilesSlider.get(), text_color=self.textColor)
+        self.amountOfFilesLabel = customtkinter.CTkLabel(self.settingsFrame, text = "2", text_color=self.textColor)
         self.amountOfFilesLabel.place(relx = 0.35, rely = 0.185)
+
+        self.newFilesList = customtkinter.CTkScrollableFrame(self.settingsFrame, width=350, height=275, fg_color=self.windowColor)
+        self.newFilesList.place(relx = 0.5, rely = 0.1)
+        
+        for i in range(25):
+            box = Frame(self.newFilesList, bg=self.secondColor, height=100, highlightbackground="black", highlightthickness=1)
+            box.pack(fill="x")
+
+            numberLabel = customtkinter.CTkLabel(box, text = i+1, font=("Arial", 18), text_color=self.textColor)
+            numberLabel.place(relx = 0.05, rely = 0.3)
+
+            selectedFilesText = customtkinter.CTkEntry(box, width=250, height=30)
+            selectedFilesText.place(relx = 0.2, rely = 0.3)
+
+        self.amountOfFilesSlider.set(2)
+        self.update_frame(2)
 
 
     def update_slider(self, event):
         amount = self.amountOfFilesSlider.get()
-        self.amountOfFilesLabel.configure(text = amount)
+        self.amountOfFilesLabel.configure(text = str(amount)[:-2])
+        self.update_frame(amount)
+
+    def update_frame(self, number):
+        l = 0
+        for i in self.newFilesList.winfo_children():
+            l += 1
+            if l > number:
+                i.pack_forget()
+
+            elif l >= number:
+                i.pack(fill="x")
